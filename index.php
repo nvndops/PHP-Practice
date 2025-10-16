6e1249bc45361d8660297bf1d240e8e514ecee52
Naveen Verma <nverma2k@gmail.com>
2025-10-16 17:13:23 +0530

    Added a new php program with the HTML code

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<!-- <?php 
 
 $name = "Naveen";
 ?>
@@ -14,5 +14,33 @@ $name = "Naveen";
     <h1> Practicing PHP integration with HTML <h1>
     
     <p> Hi <?php echo $name ?> <p>
+</body>
+</html> -->
+
+<?php 
+$hour = 12;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Exercise</title>
+</head>
+<body>
+    <h1> Practice Series </h1>
+
+    <?php if($hour < 12): ?>
+        Good Morning
+    <?php elseif ($hour < 18): ?>
+        Good Afternoon
+    <?php elseif ($hour < 22): ?>
+        Good Evening
+    <?php else: ?>
+        Good Night
+    <?php endif; ?>
+
 </body>
 </html>
