fcf6480be0e49c7c2b4ab1a45ba180e4ad3b0898
Naveen Verma <nverma2k@gmail.com>
2025-10-16 17:42:37 +0530

    New fruits program added.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!-- <?php 
+<!-- <?php
 
 $name = "Naveen";
 ?>
@@ -17,22 +17,24 @@ $name = "Naveen";
 </body>
 </html> -->
 
-<?php 
+<!-- <?php
 $hour = 12;
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Exercise</title>
 </head>
+
 <body>
     <h1> Practice Series </h1>
 
-    <?php if($hour < 12): ?>
+    <?php if ($hour < 12): ?>
         Good Morning
     <?php elseif ($hour < 18): ?>
         Good Afternoon
@@ -42,5 +44,29 @@ $hour = 12;
         Good Night
     <?php endif; ?>
 
+</body>
+
+</html> -->
+
+<?php 
+$fruits = ["apple", "banana", "orange", "mango"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fruits</title>
+</head>
+<body>
+    <h1>Fruits list</h1>
+
+    <ol>
+    <?php 
+        foreach($fruits as $fruit) {
+            echo "<li>$fruit</li>";
+        }
+    ?>
+    </ol>
 </body>
 </html>
