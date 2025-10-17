e2c0a5bc63786adbd391a8cfc1917815cc0cf1ec
Naveen Verma <nverma2k@gmail.com>
2025-10-17 17:59:03 +0530

    Added intro to HTML forms dir and content

---
+++ Day 2/Intro to HTML forms/form.php
@@ -0,0 +1,28 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    var_dump($_POST);
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <form method="post">
+
+        <input name="username">
+
+        <input name="password" type="password">
+
+        <button>Send</button>
+
+    </form>
+</body>
+</html>
